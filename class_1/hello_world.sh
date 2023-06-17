a8c206cc64c5684c35703104e0b8d22ab15a8d0c
fahriddin <fahriddin24@gmail.com>
2023-06-17 17:57:23 +0000

    initial commit

---
+++ class_1/hello_world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# echo "Hello World $$" ## $$ is t oprint out the PID
+
+echo "$1 is currently eating $2, at the same time drinking $3 -- Total # of arguments: $@"
+
+echo "$0  has input of $# arguments."
+
